ce757ec7638ad24b5651891fbb7d140e4ef08aa0
AlexeySibirtsev <s.lexx30@gmail.com>
2024-10-27 21:24:39 +0300

    add null-constraint for answers-table

---
+++ db/migrate/20241027182222_add_answers_body_question_id_null_constraint.rb
@@ -0,0 +1,6 @@
+class AddAnswersBodyQuestionIdNullConstraint < ActiveRecord::Migration[6.1]
+  def change
+    change_column_null(:answers, :body, false)
+    change_column_null(:answers, :question_id, false)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_27_181807) do
+ActiveRecord::Schema.define(version: 2024_10_27_182222) do
 
   create_table "answers", force: :cascade do |t|
-    t.string "body"
-    t.integer "question_id"
+    t.string "body", null: false
+    t.integer "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
