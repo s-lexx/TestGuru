a3f09b136ecdd35e34dc40f553c4a0cb0932fc5a
AlexeySibirtsev <s.lexx30@gmail.com>
2024-10-27 21:28:51 +0300

    add null-constraint for categories-table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_27_182544) do
+ActiveRecord::Schema.define(version: 2024_10_27_182801) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2024_10_27_182544) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
