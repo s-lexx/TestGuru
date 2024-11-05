7f4931af390b69cc0634075509870cf280d4ef52
AlexeySibirtsev <s.lexx30@gmail.com>
2024-11-05 09:25:48 +0300

    add correct attribute to answers and set default value

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_11_05_061757) do
+ActiveRecord::Schema.define(version: 2024_11_05_062234) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.integer "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "correct", default: false
   end
 
   create_table "categories", force: :cascade do |t|
