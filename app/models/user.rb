3ff9d5e2ae886df2275dfdaea8629c70653d29e8
AlexeySibirtsev <s.lexx30@gmail.com>
2025-01-01 21:20:48 +0300

    feat: add method find_by_level: find tests by user and test level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  def find_by_level(level)
+    Result
+      .joins("JOIN tests ON tests.id = results.test_id AND tests.level = ?", level)
+      .where('results.user_id = ?', id)
+      .where('results.status IS NOT NULL')
+      .pluck('tests.title')
+  end
 end
