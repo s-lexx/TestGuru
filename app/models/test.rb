ccd3f78b2b9be98c5b0b5cd03203bb7777d04ec7
AlexeySibirtsev <s.lexx30@gmail.com>
2025-01-09 09:14:24 +0300

    feat: add method find_by_name: find tests by category-name

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,10 @@
 class Test < ApplicationRecord
+  def self.find_by_name(name)
+    Test
+      .select(:title)
+      .joins("JOIN categories ON tests.category_id = categories.id")
+      .where(categories: { title: name })
+      .order('tests.title DESC')
+      .pluck('tests.title')
+  end
 end
