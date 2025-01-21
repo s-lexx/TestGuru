0fff9165ffe2b14002d8d1b06e815623bcc683de
AlexeySibirtsev <s.lexx30@gmail.com>
2025-01-21 22:36:02 +0300

    refactor: Test model

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,6 @@
 class Test < ApplicationRecord
   def self.find_by_category(name)
     Test
-      .select(:title)
       .joins("JOIN categories ON tests.category_id = categories.id")
       .where(categories: { title: name })
       .order(title: :desc)
