822bec42cb036fe34e54b9a253718a970593369d
AlexeySibirtsev <s.lexx30@gmail.com>
2025-01-21 22:32:38 +0300

    refactor: Test and User models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,10 +1,10 @@
 class Test < ApplicationRecord
-  def self.find_by_name(name)
+  def self.find_by_category(name)
     Test
       .select(:title)
       .joins("JOIN categories ON tests.category_id = categories.id")
       .where(categories: { title: name })
-      .order('tests.title DESC')
-      .pluck('tests.title')
+      .order(title: :desc)
+      .pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   def find_by_level(level)
     Test
       .joins("JOIN results ON tests.id = results.test_id")
-      .where("results.user_id = ?", id)
+      .where(results: { user_id: id })
       .where(level: level)
   end
 end
