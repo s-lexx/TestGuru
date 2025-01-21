7065f441396b32221499f43d03e64af47ae9318c
AlexeySibirtsev <s.lexx30@gmail.com>
2025-01-21 22:20:02 +0300

    fix: update find_by_level method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   def find_by_level(level)
-    Result
-      .joins("JOIN tests ON tests.id = results.test_id AND tests.level = ?", level)
-      .where('results.user_id = ?', id)
-      .where('results.status IS NOT NULL')
-      .pluck('tests.title')
+    Test
+      .joins("JOIN results ON tests.id = results.test_id")
+      .where("results.user_id = ?", id)
+      .where(level: level)
   end
 end
